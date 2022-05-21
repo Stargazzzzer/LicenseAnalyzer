54b72693d7ad72f001253073e394122f55ecd178
Stargazer <12012524@mail.sustech.edu.cn>
2022-05-21 11:20:55 +0800

    init

---
+++ src/main/java/com/sustech/licenseanalyzer/LicenseAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.sustech.licenseanalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LicenseAnalyzerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LicenseAnalyzerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sustech/licenseanalyzer/LicenseAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sustech.licenseanalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LicenseAnalyzerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
