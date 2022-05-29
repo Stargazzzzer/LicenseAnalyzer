b62adb53f20a86074e293b50088f8f7788f4e2e7
Stargazer <12012524@mail.sustech.edu.cn>
2022-05-29 23:49:51 +0800

    updated getInfoByLicense()

--- src/main/java/com/sustech/licenseanalyzer/service/InfoService.java
+++ src/main/java/com/sustech/licenseanalyzer/service/InfoService.java
@@ -57,9 +57,10 @@ public class InfoService {
         select("""
                         select pt.name, html_url, description, created_time, stargazers_count, forks_count, pt.license, array_to_string(ARRAY(SELECT unnest(array_agg(distinct(topic)))),' ')
                                from project_info join project_topic pt on project_info.name = pt.name
-                        where pt.license = ?\s
+                        where pt.license = ?
                         group by pt.name, html_url, description, created_time, stargazers_count, forks_count, pt.license
-                        order by stargazers_count desc;""",
+                        order by stargazers_count desc
+                        limit 50;""",
                 (stmt) -> stmt.setString(1, license),
                 (resultSet) -> {
                     Info i = new Info();
