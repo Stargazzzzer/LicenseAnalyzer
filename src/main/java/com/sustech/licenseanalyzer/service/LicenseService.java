1be3be4ee972f30a8a844edb72cc9decfc3db150
Stargazer <12012524@mail.sustech.edu.cn>
2022-05-30 09:54:27 +0800

    excluded unlicense

--- src/main/java/com/sustech/licenseanalyzer/service/LicenseService.java
+++ src/main/java/com/sustech/licenseanalyzer/service/LicenseService.java
@@ -14,10 +14,10 @@ public class LicenseService {
         List<Counter> res = new ArrayList<>();
         select("""
                         select license, count(license) cnt
-                        from project_info
-                        where license != 'null'
-                        group by license
-                        order by cnt desc;""",
+                                          from project_info
+                                          where license != 'null' and license != 'unlicense'
+                                          group by license
+                                          order by cnt desc;""",
                 (resultSet) -> {
                     Counter c = new Counter();
                     Counter.getCounterFromResultSet(c, resultSet);
@@ -30,18 +30,18 @@ public class LicenseService {
         List<LicenseTopic> res = new ArrayList<>();
         select("""
                         select license,
-                        array_to_string(ARRAY(SELECT unnest(array_agg(distinct (topic)))), ' ')
-                        from (select license, topic
-                        from (select license,
-                             topic,
-                             row_number() over (partition by license order by count(topic) desc) as rnk
-                        from project_topic
-                        where license != 'null'
-                             and topic != 'java'
-                             group by license, topic) tmp
-                        where rnk <= 3
-                        order by license) tmp
-                        group by license;""",
+                                                array_to_string(ARRAY(SELECT unnest(array_agg(distinct (topic)))), ' ')
+                                         from (select license, topic
+                                               from (select license,
+                                                            topic,
+                                                            row_number() over (partition by license order by count(topic) desc) as rnk
+                                                     from project_topic
+                                                     where license != 'null' and license != 'unlicense'
+                                                       and topic != 'java'
+                                                     group by license, topic) tmp
+                                               where rnk <= 3
+                                               order by license) tmp
+                                         group by license;""",
                 resultSet -> {
                     LicenseTopic lp = new LicenseTopic();
                     getLicenseTopicFromResultSet(lp, resultSet);
@@ -55,7 +55,7 @@ public class LicenseService {
         select("""
                         select license, sum(stargazers_count) as stars
                                                           from project_info
-                                                          where license != 'null'
+                                                          where license != 'null' and license != 'unlicense'
                                                           group by license
                                                           order by stars desc
                                                           limit 10;""",
@@ -72,7 +72,7 @@ public class LicenseService {
         select("""
                         select license, sum(forks_count) as forks
                                                                    from project_info
-                                                                   where license != 'null'
+                                                                   where license != 'null' and license != 'unlicense'
                                                                    group by license
                                                                    order by forks desc
                                                                    limit 10;""",
