8021142313d094ad9e345685ed6506be013e0427
wcvanvan <1057736842@qq.com>
2022-05-31 11:36:48 +0800

    fix table alignment

--- src/frontend/src/components/MyTable.vue
+++ src/frontend/src/components/MyTable.vue
@@ -1,9 +1,9 @@
 <template>
   <!-- <div style="color: rgb(224, 243, 198);"> -->
   <el-table :data="tableData" style="width: 100%;color: rgb(53, 92, 189)">
-    <el-table-column prop="license" label="License" width="180" />
-    <el-table-column prop="1" label="#1" width="180" />
-    <el-table-column prop="2" label="#2" />
+    <el-table-column prop="license" label="License"  />
+    <el-table-column prop="1" label="#1"  />
+    <el-table-column prop="2" label="#2"  />
     <el-table-column prop="3" label="#3" />
   </el-table>
   <!-- </div> -->
